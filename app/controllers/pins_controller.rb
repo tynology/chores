1f20ce6d567b9aee1ab06fadd01b1a0d3cf5e90f
Tyler McElhaney <tmac@zcodemedia.com>
2015-04-30 16:02:38 -0700

    initial commit

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,68 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all
+  end
+
+  def show
+  end
+
+  def new
+    @pin = current_user.pins.build
+  end
+
+  def edit
+  end
+
+  def create
+    @pin = current_user.pins.build(pin_params)
+
+    respond_to do |format|
+      if @pin.save
+        format.html { redirect_to @pin, notice: 'Pin was created.' }
+        format.json { render :show, status: :created, location: @pin }
+      else
+        format.html { render :new }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @pin.update(pin_params)
+        format.html { redirect_to @pin, notice: 'Pin was updated.' }
+        format.json { render :show, status: :ok, location: @pin }
+      else
+        format.html { render :edit }
+        format.json { render json: @pin.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @pin.destroy
+    respond_to do |format|
+      format.html { redirect_to pins_url, notice: 'Pin was deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+    
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "You cannot do that" if @pin.nil?
+    end
+
+    def pin_params
+      params.require(:pin).permit(:description, :image)
+    end
+end
